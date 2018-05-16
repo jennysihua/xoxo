ab8ac36bb2d7496f54adc23b8a76e5c133a24096
Zac Peires <zacpeires@googlemail.com>
2018-05-16 12:41:28 -0400

    some progress?

--- game/index.js
+++ game/index.js
@@ -1,6 +1,40 @@
 import {Map} from 'immutable'
+import {createStore} from 'redux'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map()
+
+const MOVE = 'move'
+
+
+board = board.setIn([1, 1], 'X')
+console.log(board)
+board = board.setIn([2, 1], 'Y')
+
+console.log(board)
+
+const move = (turn, rowCol) => ({
+  type: MOVE,
+  position: rowCol,
+  turn
+})
+
+
+
+export default function reducer(state = { board, turn: 'X' }, action) {
+  let newTurn;
+  if (action.turn === 'X') {
+    newTurn = 'Y'
+  } else {
+    newTurn = 'X'
+  }
+
+  // Switch(action.type)
+
+
+  return { board: board.setIn(action.position, turn),  turn: newTurn}
+
+
+
 }
+
+
